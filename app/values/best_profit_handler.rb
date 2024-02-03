ac3f2a77729af12dec903806a266627f2344641b
stephanie rousset <strabon@club-internet.fr>
2024-02-03 14:48:36 +0100

    refacto best_profit_handler

--- app/values/best_profit_handler.rb
+++ app/values/best_profit_handler.rb
@@ -7,17 +7,14 @@ class BestProfitHandler
 
   def get_best_profit
     result = 0
-    @array.each_with_index do |value, index|
-      buying_value = BigDecimal(value.to_s)
-      # iterate on each following value of buying_value
-      for i in index..(@array.length - 1)
-        selling_value = @array[i + 1]
-        next if selling_value.nil?
-        selling_value = BigDecimal(selling_value.to_s)
-        # skip if following value is less or equal buying_value
-        next if buying_value >= selling_value
-        # update best profit if it is superior
-        result = (selling_value - buying_value) * 100 if (selling_value - buying_value) * 100 > result
+    min_price = BigDecimal(@array[0].to_s)
+    @array.each do |value|
+      # update min_price if current value is less than min_price
+      if BigDecimal(value.to_s) < min_price
+        min_price = BigDecimal(value.to_s)
+      # update result if (current value - min_price) * 100 is superior to current result
+      elsif (BigDecimal(value.to_s) - min_price) * 100 > result
+        result = (BigDecimal(value.to_s) - min_price) * 100
       end
     end
     result
